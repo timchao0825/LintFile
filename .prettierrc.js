a741682bfb62b23292579b9f751c2f9844c3e194
timchao <timchao@cw.com.tw>
2021-08-10 16:49:06 +0800

    [FIXED] prettier conflict setting

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,7 +4,6 @@ module.exports = {
   semi: false,
   singleQuote: true,
   arrowParens: 'avoid',
-  trailingComma: 'all', // 在物件或陣列最後一個元素後面是否加逗號
   bracketSpacing: true, // 物件中列印空格
   ignorePath: '.prettierignore',
-};
+}
