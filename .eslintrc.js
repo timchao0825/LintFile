5abceeb5e0471df74be201cc5ee3d93a7cfab283
Tim <tim.chao0825@gmail.com>
2022-11-09 00:56:57 +0800

    update comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
+  // 指定目錄，設置root代表.eslintrc.js放置地方開始檢查
   root: true,
+  // 在哪個環境底下運行
   env: {
     commonjs: true,
     browser: true,
     node: true,
     es6: true
   },
+  // 解析用哪種程式碼
   parserOptions: {
     parser: 'babel-eslint'
   },
+  // plugins 第三方的套件，這些套件前需要先透過 npm 進行安裝
   // extends: ['eslint:recommended', 'plugin:vue/recommended', 'standard'],
+  // plugins 第三方的套件，這些套件前需要先透過 npm 進行安裝
   extends: ['eslint:recommended', 'standard'],
+  // 客製化並覆寫 lint 規則
   rules: {
     'comma-dangle': ['error', 'only-multiline'],
     'space-before-function-paren': [
